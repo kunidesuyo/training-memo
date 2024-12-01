df3d9d668177881613417b009b6fafc8c9fc11d4
kuni <kunidesuyo1234@gmail.com>
2024-12-01 17:22:39 +0900

    exercises一覧→詳細導線作成

--- app/exercises/page.tsx
+++ app/exercises/page.tsx
@@ -1,14 +1,17 @@
+import Link from "next/link";
+
 export default async function exercises() {
   const exercises = await fetch('http://localhost:3000/api/exercises').then((res) => res.json());
   return (
     <div>
       {exercises.map((exercise: any) => {
         return (
-          <div>
+          <div key={exercise.id}>
             <h2>{exercise.name}</h2>
             <p>{exercise.weight}</p>
             <p>{exercise.rep}</p>
             <p>{exercise.date}</p>
+            <Link href={`/exercises/${exercise.id}`}>詳細</Link>
           </div>
         );
       })}
