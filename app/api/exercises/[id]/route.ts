9edaf322ae04fd041ad4064fae2d7552bf58bad0
kuni <kunidesuyo1234@gmail.com>
2024-12-08 11:50:52 +0900

    exercises更新API作成

--- app/api/exercises/[id]/route.ts
+++ app/api/exercises/[id]/route.ts
@@ -21,3 +21,26 @@ export async function DELETE(request: Request, { params }: { params: Promise<{ i
   });
   return Response.json(exercise);
 }
+
+export async function PATCH(request: Request, { params }: { params: Promise<{ id: string }> }): Promise<Response> {
+  const id = (await params).id;
+  const { name, weight, rep, date } = await request.json();
+  const user = await prisma.user.findFirst();
+  const exercise = await prisma.exercise.update({
+    where: {
+      id: Number(id),
+    },
+    data: {
+      name,
+      weight,
+      rep,
+      date,
+      // TODO: ログインユーザで更新する
+      // 一旦、存在する最初のユーザを取得して更新者と作成者に設定
+      updater: {
+        connect: { id: user?.id },
+      }
+    },
+  });
+  return Response.json(exercise);
+}
