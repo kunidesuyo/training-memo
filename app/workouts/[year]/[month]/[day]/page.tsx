6ebd9473df69b9fba4d7aa4842420e66b220b2f9
kuni <kunidesuyo1234@gmail.com>
2024-12-29 21:57:23 +0900

    workout画面仮作成

--- app/workouts/[year]/[month]/[day]/page.tsx
+++ app/workouts/[year]/[month]/[day]/page.tsx
@@ -1,9 +1,14 @@
+import Link from "next/link";
+
 export default async function workout({ params }: { params: { year: string, month: string, day: string } }) {
   const {year, month, day} = await params;
   const workout = await fetch(`http://localhost:3000/api/workouts/${year}/${month}/${day}`).then((res) => res.json());
   return (
     <div>
       <h2>{`${workout.year}年${workout.month}月${workout.day}日のワークアウト`}</h2>
+      {workout.exercises.map((exercise) => (
+        <Link href={`/workouts/${year}/${month}/${day}/exercises/${exercise.id}`}>{exercise.name}</Link>
+      ))}
     </div>
   );
 }
