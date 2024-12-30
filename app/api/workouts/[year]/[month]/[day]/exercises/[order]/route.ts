6203d120f006fd2913b5ba22c7e86019ba05c050
kuni <kunidesuyo1234@gmail.com>
2024-12-30 21:22:27 +0900

    エクササイズ編集API作成

--- app/api/workouts/[year]/[month]/[day]/exercises/[order]/route.ts
+++ app/api/workouts/[year]/[month]/[day]/exercises/[order]/route.ts
@@ -35,3 +35,81 @@ export async function GET(request: Request, { params }: { params: Promise<{ year
   });
   return Response.json(exercise);
 }
+
+export async function PATCH(request: Request, { params }: { params: Promise<{ year: string, month: string, day: string, order: string }> }): Promise<Response> {
+  const { year, month, day, order } = await params;
+  // TODO: ログインユーザのIDで取得するようにする
+  const currenUserId = 1;
+  const data = await request.json();
+
+  // 対象エクササイズのIDを取得
+  const exercise = await prisma.exercise.findFirst({
+    where: {
+      workout: {
+        year: parseInt(year),
+        month: parseInt(month),
+        day: parseInt(day),
+        authorId: currenUserId,
+      },
+      order: parseInt(order),
+    },
+  });
+  const exerciseId = exercise.id;
+
+  // エクササイズに紐づくセットとレストを洗い替える
+  await prisma.$transaction([
+    prisma.set.deleteMany({
+      where: {
+        exerciseId,
+      },
+    }),
+    prisma.rest.deleteMany({
+      where: {
+        exerciseId,
+      },
+    }),
+    prisma.set.createMany({
+      data: data.sets.map((set) => {
+        return {
+          rep: set.rep,
+          weight: set.weight,
+          order: set.order,
+          exerciseId,
+        }
+      }),
+    }),
+    prisma.rest.createMany({
+      data: data.rests.map((rest) => {
+        return {
+          time: rest.time,
+          order: rest.order,
+          exerciseId,
+        }
+      })
+    }),
+  ]);
+  
+  const updatedExercise = await prisma.exercise.findFirst({
+    where: {
+      id: exerciseId,
+    },
+    include: {
+      sets: {
+        select: {
+          weight: true,
+          rep: true,
+          order: true
+        }
+      },
+      rests: {
+        select: {
+          time: true,
+          order: true
+        }
+      }
+    }
+  });
+
+  return Response.json(updatedExercise);
+}
+  
