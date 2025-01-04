fb6720d3b23d646e68fdaa6ba5b0bc2e5909bac9
kuni <kunidesuyo1234@gmail.com>
2025-01-04 18:53:23 +0900

    ログアウトボタン導入

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,14 +1,24 @@
-import { signIn } from "@/auth"
+import { signIn, signOut } from "@/auth"
  
 export default function SignIn() {
   return (
-    <form
-      action={async () => {
-        "use server"
-        await signIn("google")
-      }}
-    >
-      <button type="submit">Signin with Google</button>
-    </form>
+    <div>
+      <form
+        action={async () => {
+          "use server"
+          await signIn("google")
+        }}
+        >
+        <button type="submit">Signin with Google</button>
+      </form>
+      <form
+        action={async () => {
+          "use server"
+          await signOut()
+        }}
+      >
+        <button type="submit">Sign Out</button>
+      </form>
+    </div>
   )
 } 
