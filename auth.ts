b706786312e5193a33a0e756196fad34c2bdc269
kuni <kunidesuyo1234@gmail.com>
2025-01-05 21:29:36 +0900

    session管理をjwtに指定

--- auth.ts
+++ auth.ts
@@ -5,5 +5,8 @@ import { prisma } from "@/prisma"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "jwt",
+  },
   providers: [Google],
 });
