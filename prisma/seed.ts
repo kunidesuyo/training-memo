470ed78e84d819ef92a1a40df7c3c97a2e539948
kuni <kunidesuyo1234@gmail.com>
2025-01-07 22:46:36 +0900

    seeder修正

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,10 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 async function main() {
   const rootUser = await prisma.user.create({
     data: {
-      email: 'test@example.com',
-      name: 'root_user',
+      email: "test@example.com",
+      name: "root_user",
     },
   });
   const workout1 = await prisma.workout.create({
@@ -17,59 +17,59 @@ async function main() {
   });
   const exercise1_1 = await prisma.exercise.create({
     data: {
-      name: 'スクワット',
+      name: "スクワット",
       workoutId: workout1.id,
       order: 1,
       authorId: rootUser.id,
-    }
+    },
   });
-  const set1_1 = await prisma.exerciseItem.create({
+  await prisma.exerciseItem.create({
     data: {
-      type: 'WORK',
+      type: "WORK",
       weight: 60,
       rep: 10,
       order: 1,
       exerciseId: exercise1_1.id,
       authorId: rootUser.id,
-    }
+    },
   });
-  const rest1_1 = await prisma.exerciseItem.create({
+  await prisma.exerciseItem.create({
     data: {
-      type: 'REST',
+      type: "REST",
       time: 120,
       order: 2,
       exerciseId: exercise1_1.id,
       authorId: rootUser.id,
-    }
+    },
   });
-  const set1_2 = await prisma.exerciseItem.create({
+  await prisma.exerciseItem.create({
     data: {
-      type: 'WORK',
+      type: "WORK",
       weight: 80,
       rep: 10,
       order: 3,
       exerciseId: exercise1_1.id,
       authorId: rootUser.id,
-    }
+    },
   });
-  const rest1_2 = await prisma.exerciseItem.create({
+  await prisma.exerciseItem.create({
     data: {
-      type: 'REST',
+      type: "REST",
       time: 120,
       order: 4,
       exerciseId: exercise1_1.id,
       authorId: rootUser.id,
-    }
+    },
   });
-  const set1_3 = await prisma.exerciseItem.create({
+  await prisma.exerciseItem.create({
     data: {
-      type: 'WORK',
+      type: "WORK",
       weight: 100,
       rep: 10,
       order: 5,
       exerciseId: exercise1_1.id,
       authorId: rootUser.id,
-    }
+    },
   });
   const workout2 = await prisma.workout.create({
     data: {
@@ -81,68 +81,68 @@ async function main() {
   });
   const exercise2_1 = await prisma.exercise.create({
     data: {
-      name: 'デッドリフト',
+      name: "デッドリフト",
       workoutId: workout2.id,
       order: 1,
       authorId: rootUser.id,
-    }
+    },
   });
-  const set2_1 = await prisma.exerciseItem.create({
+  await prisma.exerciseItem.create({
     data: {
-      type: 'WORK',
+      type: "WORK",
       weight: 80,
       rep: 10,
       order: 1,
       exerciseId: exercise2_1.id,
       authorId: rootUser.id,
-    }
+    },
   });
-  const rest2_1 = await prisma.exerciseItem.create({
+  await prisma.exerciseItem.create({
     data: {
-      type: 'REST',
+      type: "REST",
       time: 120,
       order: 2,
       exerciseId: exercise2_1.id,
       authorId: rootUser.id,
-    }
+    },
   });
-  const set2_2 = await prisma.exerciseItem.create({
+  await prisma.exerciseItem.create({
     data: {
-      type: 'WORK',
+      type: "WORK",
       weight: 100,
       rep: 10,
       order: 3,
       exerciseId: exercise2_1.id,
       authorId: rootUser.id,
-    }
+    },
   });
-  const rest2_2 = await prisma.exerciseItem.create({
+  await prisma.exerciseItem.create({
     data: {
-      type: 'REST',
+      type: "REST",
       time: 120,
       order: 4,
       exerciseId: exercise2_1.id,
       authorId: rootUser.id,
-    }
+    },
   });
-  const set2_3 = await prisma.exerciseItem.create({
+  await prisma.exerciseItem.create({
     data: {
-      type: 'WORK',
+      type: "WORK",
       weight: 120,
       rep: 10,
       order: 5,
       exerciseId: exercise2_1.id,
       authorId: rootUser.id,
-    }
+    },
   });
 }
 
-// main()
-//   .then(async () => {
-//     await prisma.$disconnect();
-//   })
-//   .catch(async (e) => {
-//     console.error(e);
-//     await prisma.$disconnect();
-//     process.exit(1);
-//   });
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
