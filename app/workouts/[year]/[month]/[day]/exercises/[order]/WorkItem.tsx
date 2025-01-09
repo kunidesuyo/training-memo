c944ab5d601d4f7ca476c07f32d09af9e0545a46
kuni <kunidesuyo1234@gmail.com>
2025-01-09 20:29:04 +0900

    コンポーネントをshadcnに置き換え

--- app/workouts/[year]/[month]/[day]/exercises/[order]/WorkItem.tsx
+++ app/workouts/[year]/[month]/[day]/exercises/[order]/WorkItem.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { ExerciseItem } from "@/app/api/workouts/[year]/[month]/[day]/exercises/[order]/route";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 interface WorkItemProps {
   item: ExerciseItem;
@@ -22,21 +24,21 @@ const WorkItem: React.FC<WorkItemProps> = ({
     <div className="mb-4 mt-4" key={item.order}>
       <p>{`ワーク`}</p>
       <p>重さ: </p>
-      <input
+      <Input
         type="number"
         value={item.weight ?? ""}
         onChange={changeWeight(item)}
       />
       <p>回数: </p>
-      <input type="number" value={item.rep ?? ""} onChange={changeRep(item)} />
+      <Input type="number" value={item.rep ?? ""} onChange={changeRep(item)} />
       <p>{item.order}</p>
-      <button
+      <Button
         type="button"
         className="mx-2 text-blue-500 underline"
         onClick={() => deleteItem(item.order)}
       >
         削除
-      </button>
+      </Button>
     </div>
   );
 };
