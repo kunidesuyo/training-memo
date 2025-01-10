2b4ab7ccbda480d19853eb3265616c721204eeb5
kuni <kunidesuyo1234@gmail.com>
2025-01-10 19:33:50 +0900

    見た目修正

--- app/workouts/[year]/[month]/[day]/exercises/[order]/RestItem.tsx
+++ app/workouts/[year]/[month]/[day]/exercises/[order]/RestItem.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { ExerciseItem } from "@/app/api/workouts/[year]/[month]/[day]/exercises/[order]/route";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
 
 interface RestItemProps {
   item: ExerciseItem;
@@ -17,24 +18,26 @@ const RestItem: React.FC<RestItemProps> = ({
   deleteItem,
 }) => {
   return (
-    <div className="my-4 flex" key={item.order}>
-      <p>{`レスト`}</p>
-      <Input
-        className="mx-2 w-100"
-        type="number"
-        value={item.time ?? ""}
-        onChange={changeTime(item)}
-      />
-      <p>秒</p>
-      <Button
-        type="button"
-        className="mx-2 text-blue-500 underline"
-        onClick={() => deleteItem(item.order)}
-      >
-        削除
-      </Button>
-      <p>{item.order}</p>
-    </div>
+    <Card className="my-4 w-[600px]" key={item.order}>
+      <CardContent className="flex p-4">
+        <p>{`レスト`}</p>
+        <Input
+          className="mx-2 w-100"
+          type="number"
+          value={item.time ?? ""}
+          onChange={changeTime(item)}
+        />
+        <p>秒</p>
+        <Button
+          type="button"
+          className="mx-2 text-blue-500 underline"
+          onClick={() => deleteItem(item.order)}
+        >
+          削除
+        </Button>
+        <p>{item.order}</p>
+      </CardContent>
+    </Card>
   );
 };
 
--- app/workouts/[year]/[month]/[day]/exercises/[order]/WorkItem.tsx
+++ app/workouts/[year]/[month]/[day]/exercises/[order]/WorkItem.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { ExerciseItem } from "@/app/api/workouts/[year]/[month]/[day]/exercises/[order]/route";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
 
 interface WorkItemProps {
   item: ExerciseItem;
@@ -21,33 +22,35 @@ const WorkItem: React.FC<WorkItemProps> = ({
   deleteItem,
 }) => {
   return (
-    <div className="my-4 flex" key={item.order}>
-      <p>{`ワーク`}</p>
-      <Input
-        className="mx-2 w-100"
-        type="number"
-        value={item.weight ?? ""}
-        onChange={changeWeight(item)}
-        placeholder="重さ"
-      />
-      <p>kg</p>
-      <Input
-        className="mx-2 w-100"
-        type="number"
-        value={item.rep ?? ""}
-        onChange={changeRep(item)}
-        placeholder="回数"
-      />
-      <p>回</p>
-      <Button
-        type="button"
-        className="mx-2 text-blue-500 underline"
-        onClick={() => deleteItem(item.order)}
-      >
-        削除
-      </Button>
-      <p>{item.order}</p>
-    </div>
+    <Card className="my-4 w-[600px]" key={item.order}>
+      <CardContent className="flex p-4">
+        <p>{`ワーク`}</p>
+        <Input
+          className="mx-2 w-100"
+          type="number"
+          value={item.weight ?? ""}
+          onChange={changeWeight(item)}
+          placeholder="重さ"
+        />
+        <p>kg</p>
+        <Input
+          className="mx-2 w-100"
+          type="number"
+          value={item.rep ?? ""}
+          onChange={changeRep(item)}
+          placeholder="回数"
+        />
+        <p>回</p>
+        <Button
+          type="button"
+          className="mx-2 text-blue-500 underline"
+          onClick={() => deleteItem(item.order)}
+        >
+          削除
+        </Button>
+        <p>{item.order}</p>
+      </CardContent>
+    </Card>
   );
 };
 
