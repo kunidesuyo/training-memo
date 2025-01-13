393a5bc940e7dfe77294a2840c790f6d8140565e
kuni <kunidesuyo1234@gmail.com>
2025-01-13 22:31:03 +0900

    カレンダー見た目修正

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,17 +3,17 @@
 import React, { useState } from 'react'
 import { Calendar } from "@/components/ui/calendar"
 
-
 export default function Page() {
   const [date, setDate] = useState<Date | undefined>(new Date())
   return (
-    <div>
-      <Calendar
-        mode='single'
-        selected={date}
-        onSelect={setDate}
-        className='rounded-md border'
-      />
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className="my-4 rounded-md border p-4">
+        <Calendar
+          mode='single'
+          selected={date}
+          onSelect={setDate}
+        />
+      </div>
       <div>カレンダーでクリックしたワークアウトの概要表示</div>
     </div>
   )
