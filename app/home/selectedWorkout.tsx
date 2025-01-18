d80d69b620b73366ccfd82426ffadc73b4533846
kuni <kunidesuyo1234@gmail.com>
2025-01-18 19:43:38 +0900

    選択した日付のワークアウトを表示

--- app/home/selectedWorkout.tsx
+++ app/home/selectedWorkout.tsx
@@ -3,14 +3,24 @@
 import { Workout } from "@/app/home/page";
 import React, { useEffect, useState } from "react";
 
+type WorkoutWithExercise = Workout & {
+  exercises: Exercise[];
+};
+
+type Exercise = {
+  id: number;
+  name: string;
+  order: number;
+};
+
 export default function SelectedWorkout({
   selectedDate,
 }: {
   selectedDate: Date | undefined;
 }) {
-  const [selectedWorkout, setSelectedWorkout] = useState<Workout | undefined>(
-    undefined
-  );
+  const [selectedWorkout, setSelectedWorkout] = useState<
+    WorkoutWithExercise | undefined
+  >(undefined);
 
   // useEffect使っていいのか調べる
   useEffect(() => {
@@ -22,7 +32,7 @@ export default function SelectedWorkout({
         const response = await fetch(
           `http://localhost:3000/api/workouts/${year}/${month}/${day}`
         );
-        const workout: Workout = await response.json();
+        const workout: WorkoutWithExercise = await response.json();
         setSelectedWorkout(workout);
       }
     };
@@ -32,23 +42,31 @@ export default function SelectedWorkout({
 
   return (
     <div>
-      {selectedDate ? (
+      {/* {selectedDate ? (
         <div>
           <p>選択された日付</p>
           <p>{selectedDate.getFullYear()}年</p>
           <p>{selectedDate.getMonth() + 1}月</p>
           <p>{selectedDate.getDate()}日</p>
-          {selectedWorkout ? (
-            <div>
-              <p>ワークアウトID: {selectedWorkout.id}</p>
-              {/* 他のワークアウトの詳細をここに表示 */}
-            </div>
-          ) : (
-            <p>ワークアウトが見つかりませんでした。</p>
-          )}
         </div>
       ) : (
         <p>日付が選択されていません。</p>
+      )} */}
+      {selectedWorkout ? (
+        <div>
+          {/* <p>ワークアウトID: {selectedWorkout.id}</p> */}
+          {selectedWorkout.exercises.map(
+            (exercise: Exercise, index: number) => (
+              <div key={exercise.id}>
+                <p>
+                  エクササイズ{index + 1}: {exercise.name}
+                </p>
+              </div>
+            )
+          )}
+        </div>
+      ) : (
+        <p>ワークアウトが見つかりませんでした。</p>
       )}
     </div>
   );
