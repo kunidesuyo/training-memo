861bfa64adc4e6c3533d6917048d6316355d8f0c
kuni <kunidesuyo1234@gmail.com>
2025-01-25 21:09:09 +0900

    route handler廃止

--- app/workouts/[year]/[month]/[day]/page.tsx
+++ app/workouts/[year]/[month]/[day]/page.tsx
@@ -1,10 +1,10 @@
+import {
+  Exercise,
+  getWorkoutWithExercise,
+  WorkoutWithExercises,
+} from "@/app/workouts/[year]/[month]/[day]/actions";
 import AddExerciseForm from "@/app/workouts/[year]/[month]/[day]/addExerciseForm";
 import Link from "next/link";
-
-type Exercise = {
-  id: number;
-  name: string;
-};
 
 export default async function Workout({
   params,
@@ -12,15 +12,17 @@ export default async function Workout({
   params: { year: string; month: string; day: string };
 }) {
   const { year, month, day } = await params;
-  const workout = await fetch(
-    `http://localhost:3000/api/workouts/${year}/${month}/${day}`
-  ).then((res) => res.json());
+  const workout: WorkoutWithExercises = await getWorkoutWithExercise(
+    parseInt(year),
+    parseInt(month),
+    parseInt(day)
+  );
 
   return (
     <div>
       <h2>{`${workout.year}年${workout.month}月${workout.day}日のワークアウト`}</h2>
       <div className="flex flex-col">
-        {workout.exercises.map((exercise: Exercise) => (
+        {workout.exercises?.map((exercise: Exercise) => (
           <Link
             key={exercise.id}
             className="text-blue-500 underline mx-4 my-2"
