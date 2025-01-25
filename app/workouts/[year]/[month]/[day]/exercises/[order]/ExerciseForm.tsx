c1247632c8cb5871118d27176147ee6302ee399f
kuni <kunidesuyo1234@gmail.com>
2025-01-25 22:01:07 +0900

    TODO追加

--- app/workouts/[year]/[month]/[day]/exercises/[order]/ExerciseForm.tsx
+++ app/workouts/[year]/[month]/[day]/exercises/[order]/ExerciseForm.tsx
@@ -16,7 +16,7 @@ const ExerciseItemSchema = z.object({
   rep: z.number().nullable(),
 });
 
-// 更新処理をserverActionsで行うように修正する
+// TODO: 更新処理をserverActionsで行うように修正する
 export default function ExerciseForm({
   exercise,
   pathParams,
