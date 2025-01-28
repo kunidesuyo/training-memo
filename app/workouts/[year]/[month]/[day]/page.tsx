cbc63c628b0c208defe45ad7fc3646f28064febc
kuni <kunidesuyo1234@gmail.com>
2025-01-28 22:06:25 +0900

    accordion導入

--- app/workouts/[year]/[month]/[day]/page.tsx
+++ app/workouts/[year]/[month]/[day]/page.tsx
@@ -4,6 +4,12 @@ import {
   WorkoutWithExercises,
 } from "@/app/workouts/[year]/[month]/[day]/actions";
 import AddExerciseForm from "@/app/workouts/[year]/[month]/[day]/addExerciseForm";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 import Link from "next/link";
 
 export default async function Workout({
@@ -21,16 +27,23 @@ export default async function Workout({
   return (
     <div>
       <h2>{`${workout.year}年${workout.month}月${workout.day}日のワークアウト`}</h2>
-      <div className="flex flex-col">
-        {workout.exercises?.map((exercise: Exercise) => (
-          <Link
-            key={exercise.id}
-            className="text-blue-500 underline mx-4 my-2"
-            href={`/workouts/${year}/${month}/${day}/exercises/${exercise.order}`}
-          >
-            {exercise.name}
-          </Link>
-        ))}
+      <div>
+        <Accordion type="single" collapsible>
+          {workout.exercises?.map((exercise: Exercise) => (
+            <AccordionItem value={exercise.id.toString()}>
+              <AccordionTrigger>{exercise.name}</AccordionTrigger>
+              <AccordionContent>
+                <Link
+                  key={exercise.id}
+                  className="text-blue-500 underline mx-4 my-2"
+                  href={`/workouts/${year}/${month}/${day}/exercises/${exercise.order}`}
+                >
+                  {exercise.name}
+                </Link>
+              </AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
       </div>
       <div className="m-4">
         <AddExerciseForm pathParams={await params} />
