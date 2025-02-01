ae953c6d1ef43b422c46b2992c97d4672b3ce475
kuni <kunidesuyo1234@gmail.com>
2025-02-01 17:18:49 +0900

    エクササイズ追加をserveractions化

---
+++ app/workouts/[year]/[month]/[day]/_actions/addExercise.ts
@@ -0,0 +1,71 @@
+"use server";
+
+import { z } from "zod";
+import { prisma } from "@/prisma";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+export type State = {
+  errors?: {
+    name?: string[];
+  };
+  message?: string | null;
+};
+
+const ExerciseFormSchema = z.object({
+  name: z.string(),
+});
+
+export async function addExercise(
+  year: number,
+  month: number,
+  day: number,
+  prevState: State,
+  formData: FormData
+) {
+  const currenUserId = 1;
+  const validatedFields = ExerciseFormSchema.safeParse({
+    name: formData.get("name"),
+  });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "error",
+    };
+  }
+  const { name } = validatedFields.data;
+  // TODO: 以下のもっと綺麗に書けそう
+  const workout = await prisma.workout.findFirstOrThrow({
+    where: {
+      year: year,
+      month: month,
+      day: day,
+      authorId: currenUserId,
+    },
+    select: {
+      id: true,
+      exercises: {
+        select: {
+          order: true,
+        },
+      },
+    },
+  });
+  const workoutId = workout?.id;
+  const maxOrder = workout?.exercises.reduce(
+    (acc, cur) => Math.max(acc, cur.order),
+    0
+  );
+  const newOrder = maxOrder + 1;
+  await prisma.exercise.create({
+    data: {
+      name: name,
+      workoutId,
+      order: newOrder,
+      authorId: currenUserId,
+    },
+  });
+  revalidatePath(`/workouts/${year}/${month}/${day}`);
+  redirect(`/workouts/${year}/${month}/${day}`);
+}
--- app/workouts/[year]/[month]/[day]/addExerciseForm.tsx
+++ app/workouts/[year]/[month]/[day]/addExerciseForm.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import {
+  addExercise,
+  State,
+} from "@/app/workouts/[year]/[month]/[day]/_actions/addExercise";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useActionState } from "react";
 
 export default function AddExerciseForm({
   pathParams,
@@ -11,40 +14,32 @@ export default function AddExerciseForm({
   pathParams: { year: string; month: string; day: string };
 }) {
   const { year, month, day } = pathParams;
-  const [exerciseName, setExerciseName] = useState("");
-  const router = useRouter();
-
-  const addExercise = async (event: React.FormEvent) => {
-    event.preventDefault();
-    const response = await fetch(
-      `http://localhost:3000/api/workouts/${year}/${month}/${day}/exercises`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          name: exerciseName,
-        }),
-      }
-    );
-    if (response.ok) {
-      setExerciseName("");
-      router.push(`/workouts/${year}/${month}/${day}`);
-    } else {
-      alert("Failed to update exercise.");
-    }
-  };
+  const initialState: State = { message: null, errors: {} };
+  const addExercise_ = addExercise.bind(
+    null,
+    parseInt(year),
+    parseInt(month),
+    parseInt(day)
+  );
+  const [state, formAction] = useActionState(addExercise_, initialState);
 
   return (
-    <form onSubmit={addExercise} className="flex items-center">
+    <form action={formAction} className="flex items-center">
       <Input
         className="w-100"
         type="text"
+        name="name"
         placeholder="エクササイズ名"
-        value={exerciseName}
-        onChange={(e) => setExerciseName(e.target.value)}
+        defaultValue={""}
       />
+      <div id="weight-error" aria-live="polite" aria-atomic="true">
+        {state.errors?.name &&
+          state.errors.name.map((error: string) => (
+            <p className="mt-2 text-sm text-red-500" key={error}>
+              {error}
+            </p>
+          ))}
+      </div>
       <Button type="submit" className="mx-2 underline">
         エクササイズ追加
       </Button>
