afb60f25c0ee718a49e58d2e1be05fc6a483c312
kuni <kunidesuyo1234@gmail.com>
2025-02-11 10:19:57 +0900

    リファクタリング

--- app/home/selectedWorkout.tsx
+++ app/home/selectedWorkout.tsx
@@ -7,45 +7,65 @@ import CreateWorkoutButton from "@/app/home/createWorkoutButton";
 import Link from "next/link";
 import React from "react";
 
+const formatDate = (selectedDate: Date) => {
+  const year = selectedDate.getFullYear();
+  const month = selectedDate.getMonth() + 1;
+  const day = selectedDate.getDate();
+  return { year, month, day };
+};
+
+const renderSelectedDate = (selectedDate: Date) => {
+  const { year, month, day } = formatDate(selectedDate);
+  return (
+    <div>
+      <p>選択された日付</p>
+      <p>
+        {year}年{month}月{day}日
+      </p>
+    </div>
+  );
+};
+
+const renderWorkoutDetails = (
+  selectedWorkout: WorkoutWithExercises,
+  year: number,
+  month: number,
+  day: number
+) => {
+  return (
+    <div>
+      {selectedWorkout.exercises?.map((exercise: Exercise, index: number) => (
+        <div key={exercise.id}>
+          <p>
+            エクササイズ{index + 1}: {exercise.name}
+          </p>
+        </div>
+      ))}
+      <Link href={`/workouts/${year}/${month}/${day}/`}>
+        ワークアウト詳細へ
+      </Link>
+    </div>
+  );
+};
+
 export default async function SelectedWorkout({
   selectedDate,
 }: {
   selectedDate: Date;
 }) {
-  const year = selectedDate.getFullYear();
-  const month = selectedDate.getMonth() + 1;
-  const day = selectedDate.getDate();
+  const { year, month, day } = formatDate(selectedDate);
   const selectedWorkout: WorkoutWithExercises | null =
     await getWorkoutWithExercise(year, month, day);
 
   return (
     <div>
       {selectedDate ? (
-        <div>
-          <p>選択された日付</p>
-          <p>
-            {year}年{month}月{day}日
-          </p>
-        </div>
+        renderSelectedDate(selectedDate)
       ) : (
         <p>日付を選択してください。</p>
       )}
-
       {selectedWorkout ? (
-        <div>
-          {selectedWorkout.exercises?.map(
-            (exercise: Exercise, index: number) => (
-              <div key={exercise.id}>
-                <p>
-                  エクササイズ{index + 1}: {exercise.name}
-                </p>
-              </div>
-            )
-          )}
-          <Link href={`/workouts/${year}/${month}/${day}/`}>
-            ワークアウト詳細へ
-          </Link>
-        </div>
+        renderWorkoutDetails(selectedWorkout, year, month, day)
       ) : (
         <div>
           <p>ワークアウトが見つかりませんでした。</p>
