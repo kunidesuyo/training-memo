72086a735454e90546f012a6293f2277a07e93ed
kuni <kunidesuyo1234@gmail.com>
2025-02-16 19:03:54 +0900

    rename component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 
-export default async function Home() {
+export default async function Page() {
   const session = await auth();
   if (!session) {
     redirect("/api/auth/signin");
