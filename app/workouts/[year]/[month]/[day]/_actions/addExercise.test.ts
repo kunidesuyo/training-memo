c69eae2eba4ec198a0ccfa6fef6626c1eae38323
kuni <kunidesuyo1234@gmail.com>
2025-02-19 21:21:38 +0900

    addExerciseのテストケース追加

---
+++ app/workouts/[year]/[month]/[day]/_actions/addExercise.test.ts
@@ -0,0 +1,31 @@
+// import { getCurrentUser } from "@/app/_utils/getCurrentUser";
+// import { prisma } from "@/prisma";
+
+describe("validaition error test", () => {
+  it("nameが空の場合、エラーを返す", async () => {});
+});
+
+describe("addExercise test", () => {
+  it("WorkoutにExerciseを追加できる", async () => {
+  });
+
+  it("Workoutが存在しない場合、例外を返す", async () => {
+  //   const year = 2025;
+  //   const month = 1;
+  //   const day = 1;
+  //   const currentUser = getCurrentUser();
+  //   await prisma.workout.delete({
+  //     where: {
+  //       year_month_day_authorId: {
+  //         year,
+  //         month,
+  //         day,
+  //         authorId: currentUser.id,
+  //       },
+  //     },
+  //   });
+  //   await expect(getWorkout(year, month, day)).rejects.toThrow(
+  //     "No Workout found"
+    // );
+  });
+});
