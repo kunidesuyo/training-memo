cbd11235c9b585b1f912a04620785cdb989b2b81
kuni <kunidesuyo1234@gmail.com>
2025-02-20 22:38:51 +0900

    addExerciseのテストケース記述

--- app/workouts/[year]/[month]/[day]/_actions/addExercise.test.ts
+++ app/workouts/[year]/[month]/[day]/_actions/addExercise.test.ts
@@ -1,8 +1,15 @@
 // import { getCurrentUser } from "@/app/_utils/getCurrentUser";
 // import { prisma } from "@/prisma";
 
+import { addExercise } from "@/app/workouts/[year]/[month]/[day]/_actions/addExercise";
+
 describe("validaition error test", () => {
-  it("nameが空の場合、エラーを返す", async () => {});
+  it("nameが空の場合、エラーを返す", async () => {
+    const formData = new FormData();
+    formData.append("name", "");
+    const result = await addExercise(2025, 1, 1, { errors: {}, message: null }, formData);
+    expect(result.errors?.name).toEqual(["String must contain at least 1 character(s)"]);
+  });
 });
 
 describe("addExercise test", () => {
