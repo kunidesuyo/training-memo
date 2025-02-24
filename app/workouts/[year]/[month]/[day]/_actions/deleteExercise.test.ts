a446b3eba5e0620145e2f6caafe14685775f73f8
kuni <kunidesuyo1234@gmail.com>
2025-02-24 21:53:07 +0900

    deleteExerciseTest実装

---
+++ app/workouts/[year]/[month]/[day]/_actions/deleteExercise.test.ts
@@ -0,0 +1,68 @@
+import { getCurrentUser } from "@/app/_utils/getCurrentUser";
+import { deleteExercise } from "@/app/workouts/[year]/[month]/[day]/_actions/deleteExercise";
+import { prisma } from "@/prisma";
+import { faker } from "@faker-js/faker/locale/ja";
+
+beforeEach(() => {
+  vi.mock("next/cache", () => {
+    return {
+      revalidatePath: () => {
+        return;
+      },
+    };
+  });
+
+  vi.mock("next/navigation", () => {
+    return {
+      redirect: () => {
+        return;
+      },
+    };
+  });
+});
+
+describe("動作テスト", () => {
+  it("対象のExerciseを削除できる", async () => {
+    // Arrange
+    const year = faker.date.anytime().getFullYear();
+    const month = faker.date.future().getMonth();
+    const day = faker.date.future().getDate();
+    const exerciseOrder = 1;
+    const currentUser = getCurrentUser();
+    const targetWorkout = await prisma.workout.create({
+      data: {
+        year,
+        month,
+        day,
+        authorId: currentUser.id,
+        exercises: {
+          create: [
+            {
+              name: "test",
+              order: exerciseOrder,
+              authorId: currentUser.id,
+            },
+          ],
+        },
+      },
+    });
+
+    // Act
+    await deleteExercise(year, month, day, exerciseOrder);
+
+    // Assert
+    const deletedExercise = await prisma.exercise.findFirst({
+      where: {
+        workout: {
+          id: targetWorkout.id,
+        },
+        order: exerciseOrder,
+      },
+    });
+    expect(deletedExercise).toBeNull();
+  });
+
+  it("対象のWorkout(Exerciseの親)が存在しない場合、例外を返す", async () => {});
+
+  it("対象のWorkoutは存在するが、対象のExerciseが存在しない場合、例外を返す", async () => {});
+});
