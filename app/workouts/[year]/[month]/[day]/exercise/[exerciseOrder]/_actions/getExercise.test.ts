f2c29abbf376536a1b06a718547ce472ca6fcfa1
kuni <kunidesuyo1234@gmail.com>
2025-02-27 20:47:09 +0900

    getExercise test

--- app/workouts/[year]/[month]/[day]/exercise/[exerciseOrder]/_actions/getExercise.test.ts
+++ app/workouts/[year]/[month]/[day]/exercise/[exerciseOrder]/_actions/getExercise.test.ts
@@ -1,7 +1,7 @@
-// import { getCurrentUser } from "@/app/_utils/getCurrentUser";
-// import { deleteExercise } from "@/app/workouts/[year]/[month]/[day]/_actions/deleteExercise";
-// import { prisma } from "@/prisma";
-// import { faker } from "@faker-js/faker/locale/ja";
+import { getCurrentUser } from "@/app/_utils/getCurrentUser";
+import { getExercise } from "@/app/workouts/[year]/[month]/[day]/exercise/[exerciseOrder]/_actions/getExercise";
+import { prisma } from "@/prisma";
+import { faker } from "@faker-js/faker/locale/ja";
 
 beforeEach(() => {
   vi.mock("next/cache", () => {
@@ -23,11 +23,39 @@ beforeEach(() => {
 
 describe("getExercise test", () => {
   it("対象のExerciseが返される", async () => {
+    // Arrange
+    const year = faker.date.anytime().getFullYear();
+    const month = faker.date.future().getMonth();
+    const day = faker.date.future().getDate();
+    const exerciseOrder = 1;
+    const currentUser = getCurrentUser();
+    const targetWorkout = await prisma.workout.create({
+      data: {
+        year,
+        month,
+        day,
+        authorId: currentUser.id,
+        exercises: {
+          create: [
+            {
+              name: "test",
+              order: exerciseOrder,
+              authorId: currentUser.id,
+            },
+          ],
+        },
+      },
+      select: {
+        exercises: true,
+      },
+    });
 
+    // Act
+    const exercise = await getExercise(year, month, day, exerciseOrder);
+
+    // Assert
+    expect(exercise.id).toBe(targetWorkout.exercises[0].id);
   });
 
-  it("対象のExerciseが存在しない場合、例外を返す", async () => {
-
-  });
-
+  it("対象のExerciseが存在しない場合、例外を返す", async () => {});
 });
