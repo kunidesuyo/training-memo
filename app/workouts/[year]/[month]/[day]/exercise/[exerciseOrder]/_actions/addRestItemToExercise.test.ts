773e9e63ab10d77741419be5866aa743b6a54fd7
kuni <kunidesuyo1234@gmail.com>
2025-03-12 21:28:52 +0900

    addRestItemToExercise test テストケース追加

--- app/workouts/[year]/[month]/[day]/exercise/[exerciseOrder]/_actions/addRestItemToExercise.test.ts
+++ app/workouts/[year]/[month]/[day]/exercise/[exerciseOrder]/_actions/addRestItemToExercise.test.ts
@@ -72,7 +72,64 @@ describe("addRestItemToExercise test", () => {
     expect(restExerciseItem[0].time).toBe(0);
   });
 
-  it("restItemを持つExerciseにrestItemが追加できる", async () => {});
+  it("restItemを持つExerciseにrestItemが追加できる", async () => {
+    // Arrange
+    const year = faker.date.anytime().getFullYear();
+    const month = faker.date.future().getMonth();
+    const day = faker.date.future().getDate();
+    const exerciseOrder = 1;
+    const currentUser = getCurrentUser();
+    // const targetWorkout =
+    await prisma.workout.create({
+      data: {
+        year,
+        month,
+        day,
+        authorId: currentUser.id,
+        exercises: {
+          create: [
+            {
+              name: faker.lorem.words(),
+              order: exerciseOrder,
+              authorId: currentUser.id,
+              restItems: {
+                create: [
+                  {
+                    time: 10,
+                    order: 1,
+                    authorId: currentUser.id,
+                  },
+                ],
+              },
+            },
+          ],
+        },
+      },
+      select: {
+        exercises: true,
+      },
+    });
+
+    // Act
+    await addRestItemToExercise(year, month, day, exerciseOrder);
+
+    // Assert
+    const restExerciseItem = await prisma.restExerciseItem.findMany({
+      where: {
+        exercise: {
+          workout: {
+            year,
+            month,
+            day,
+            authorId: currentUser.id,
+          },
+          order: exerciseOrder,
+        },
+      },
+    });
+    expect(restExerciseItem).toHaveLength(2);
+    expect(restExerciseItem.some(item => item.order === 2)).toBe(true);
+  });
   it("workItemを持つExerciseにrestItemが追加できる", async () => {});
   it("restItem, workItem両方を持つExerciseにrestItemが追加できる", async () => {});
   it("Exerciseが存在しない場合、例外を返す", async () => {});
