6e45f90e6830a239d5fc31d94528cb242fa482b5
kuni <kunidesuyo1234@gmail.com>
2025-03-12 22:41:03 +0900

    test

--- app/workouts/[year]/[month]/[day]/exercise/[exerciseOrder]/_actions/addRestItemToExercise.test.ts
+++ app/workouts/[year]/[month]/[day]/exercise/[exerciseOrder]/_actions/addRestItemToExercise.test.ts
@@ -271,7 +271,7 @@ describe("addRestItemToExercise test", () => {
 
     // Act & Assert
     await expect(
-      addRestItemToExercise(year, month, day, exerciseOrder)
+      addRestItemToExercise(year, month, day, exerciseOrder),
     ).rejects.toThrow("No Exercise found");
   });
 });
