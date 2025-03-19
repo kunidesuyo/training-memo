569988918901e99363bbc3d9d5d1c4dbd3a1bae6
kuni <kunidesuyo1234@gmail.com>
2025-03-19 22:08:19 +0900

    actions/getWorkout　削除

--- src/app/workouts/[year]/[month]/[day]/_actions/getWorkout.ts
+++ src/app/workouts/[year]/[month]/[day]/_actions/getWorkout.ts
@@ -1,68 +1,69 @@
-import { prisma } from "@/prisma";
-import { getCurrentUser } from "@/src/app/_utils/getCurrentUser";
-import { Prisma } from "@prisma/client";
+// TODO: Serviceに移行後削除予定
+// import { prisma } from "@/prisma";
+// import { getCurrentUser } from "@/src/app/_utils/getCurrentUser";
+// import { Prisma } from "@prisma/client";
 
-const workoutValidator = Prisma.validator<Prisma.WorkoutDefaultArgs>()({
-  select: {
-    id: true,
-    exercises: {
-      select: {
-        id: true,
-        name: true,
-        workoutId: true,
-        order: true,
-        workItems: {
-          select: {
-            exerciseId: true,
-            weight: true,
-            rep: true,
-            order: true,
-          },
-          orderBy: {
-            order: "asc",
-          },
-        },
-        restItems: {
-          select: {
-            exerciseId: true,
-            time: true,
-            order: true,
-          },
-          orderBy: {
-            order: "asc",
-          },
-        },
-      },
-    },
-  },
-});
+// const workoutValidator = Prisma.validator<Prisma.WorkoutDefaultArgs>()({
+//   select: {
+//     id: true,
+//     exercises: {
+//       select: {
+//         id: true,
+//         name: true,
+//         workoutId: true,
+//         order: true,
+//         workItems: {
+//           select: {
+//             exerciseId: true,
+//             weight: true,
+//             rep: true,
+//             order: true,
+//           },
+//           orderBy: {
+//             order: "asc",
+//           },
+//         },
+//         restItems: {
+//           select: {
+//             exerciseId: true,
+//             time: true,
+//             order: true,
+//           },
+//           orderBy: {
+//             order: "asc",
+//           },
+//         },
+//       },
+//     },
+//   },
+// });
 
-export type Workout = Prisma.WorkoutGetPayload<typeof workoutValidator>;
+// export type Workout = Prisma.WorkoutGetPayload<typeof workoutValidator>;
 
-export type Exercise = Workout["exercises"][number];
+// export type Exercise = Workout["exercises"][number];
 
-export type WorkItem = Exercise["workItems"][number];
+// export type WorkItem = Exercise["workItems"][number];
 
-export type RestItem = Exercise["restItems"][number];
+// export type RestItem = Exercise["restItems"][number];
 
-// export type ExerciseItem = ExerciseWithItems["items"][number];
+// // export type ExerciseItem = ExerciseWithItems["items"][number];
 
-export async function getWorkout(
-  year: number,
-  month: number,
-  day: number,
-): Promise<Workout> {
-  const { id: currentUserId } = getCurrentUser();
-  const workout = await prisma.workout.findUniqueOrThrow({
-    where: {
-      year_month_day_authorId: {
-        year,
-        month,
-        day,
-        authorId: currentUserId,
-      },
-    },
-    select: workoutValidator.select,
-  });
-  return workout;
-}
+// export async function getWorkout(
+//   year: number,
+//   month: number,
+//   day: number,
+// ): Promise<Workout> {
+//   const { id: currentUserId } = getCurrentUser();
+//   const workout = await prisma.workout.findUniqueOrThrow({
+//     where: {
+//       year_month_day_authorId: {
+//         year,
+//         month,
+//         day,
+//         authorId: currentUserId,
+//       },
+//     },
+//     select: workoutValidator.select,
+//   });
+//   return workout;
+// }
