41472545d6db5d0b25f1f2f183595eb3f86cd999
kuni <kunidesuyo1234@gmail.com>
2025-03-23 17:24:36 +0900

    exerciseRepository.addToWorkoutのテスト作成

--- src/repositories/ExerciseRepository.test.ts
+++ src/repositories/ExerciseRepository.test.ts
@@ -2,7 +2,57 @@ import { prisma } from "@/prisma";
 import { getCurrentUser } from "@/src/app/_utils/getCurrentUser";
 import { ExerciseRepository } from "@/src/repositories/ExerciseRepository";
 
-describe.skip("addToWorkout test", () => {});
+describe("addToWorkout test", () => {
+  it("exerciseが追加されること", async () => {
+    // Arrange
+    const year = 2025;
+    const month = 1;
+    const day = 1;
+    const currentUser = getCurrentUser();
+    const workout = await prisma.workout.create({
+      data: {
+        year,
+        month,
+        day,
+        authorId: currentUser.id,
+      },
+    });
+
+    // Act
+    const exerciseRepository = new ExerciseRepository(prisma);
+    await exerciseRepository.addToWorkout(workout.id, {
+      name: "test",
+      order: 1,
+      authorId: currentUser.id,
+    });
+
+    // Assert
+    const fetchedExercise = await prisma.exercise.findUnique({
+      where: {
+        workoutId_order: {
+          workoutId: workout.id,
+          order: 1,
+        },
+      },
+    });
+    expect(fetchedExercise).not.toBeNull();
+  });
+  it("対象のworkoutが存在しない場合、例外を返すこと", async () => {
+    // Arrange
+
+    // Act
+    const exerciseRepository = new ExerciseRepository(prisma);
+
+    // Assert
+    await expect(
+      exerciseRepository.addToWorkout(1, {
+        name: "test",
+        order: 1,
+        authorId: 1,
+      }),
+    ).rejects.toThrow();
+  });
+});
 
 describe("delte test", () => {
   it("exerciseが削除されること", async () => {
