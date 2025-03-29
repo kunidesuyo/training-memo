2ab667444970f5759761abdecb9e398a33364ba3
kuni <kunidesuyo1234@gmail.com>
2025-03-29 22:04:22 +0900

    uninstall auth.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,4 @@
-import { auth } from "@/auth";
-import { redirect } from "next/navigation";
-
 export default async function Page() {
-  const session = await auth();
-  if (!session) {
-    redirect("/api/auth/signin");
-  }
   return (
     <div>
       <h1>training memo</h1>
