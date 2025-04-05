0011a191ea96772aa44c6decfbcdfcb8eb8a624a
kuni <kunidesuyo1234@gmail.com>
2025-04-05 11:01:01 +0900

    エクササイズ削除時に確認するように修正

--- src/app/workouts/[year]/[month]/[day]/exercise/[exerciseOrder]/deleteExercise.tsx
+++ src/app/workouts/[year]/[month]/[day]/exercise/[exerciseOrder]/deleteExercise.tsx
@@ -15,19 +15,22 @@ export default function DeleteExercise({
   day: number;
   exerciseOrder: number;
 }) {
+  const handleDelete = () => {
+    if (window.confirm("このエクササイズを削除してもよろしいですか？")) {
+      deleteExerciseAction(year, month, day, exerciseOrder);
+    }
+  };
+
   return (
     <div>
-      <form
-        action={() => deleteExerciseAction(year, month, day, exerciseOrder)}
+      <Button
+        onClick={handleDelete}
+        variant="ghost"
+        className="border border-gray-200 hover:bg-red-100 active:bg-red-200 duration-200 text-gray-500 hover:text-red-600 hover:shadow-sm flex items-center gap-1"
       >
-        <Button
-          type="submit"
-          variant="ghost"
-          className="border border-gray-200 hover:bg-red-100 active:bg-red-200 duration-200 text-gray-500 hover:text-red-600 hover:shadow-sm flex items-center gap-1"
-        >
-          <Trash2 className="h-4 w-4" />
-        </Button>
-      </form>
+        <Trash2 className="h-4 w-4" />
+        <span className="text-xs ml-1">削除</span>
+      </Button>
     </div>
   );
 }
