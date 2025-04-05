2a481d224124f887d264cb92a3200922eb8a241b
kuni <kunidesuyo1234@gmail.com>
2025-04-05 11:02:12 +0900

    削除ボタンの文字表示を削除

--- src/app/workouts/[year]/[month]/[day]/exercise/[exerciseOrder]/deleteExercise.tsx
+++ src/app/workouts/[year]/[month]/[day]/exercise/[exerciseOrder]/deleteExercise.tsx
@@ -29,7 +29,6 @@ export default function DeleteExercise({
         className="border border-gray-200 hover:bg-red-100 active:bg-red-200 duration-200 text-gray-500 hover:text-red-600 hover:shadow-sm flex items-center gap-1"
       >
         <Trash2 className="h-4 w-4" />
-        <span className="text-xs ml-1">削除</span>
       </Button>
     </div>
   );
