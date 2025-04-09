299f19a99ec5ff0a06e6d0564719beab7f287fdc
kuni <kunidesuyo1234@gmail.com>
2025-04-09 09:27:40 +0900

    コンポーネント単位でディレクトリを切る(pages/home)

--- src/components/pages/home/createWorkoutButton/createWorkoutButton.tsx
+++ src/components/pages/home/createWorkoutButton/createWorkoutButton.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { createWorkoutAction } from "@/src/components/pages/home/createWorkoutAction";
+import { createWorkoutAction } from "@/src/components/pages/home/createWorkoutButton/createWorkoutAction";
 
 export default function CreateWorkoutButton({
   selectedDate,
