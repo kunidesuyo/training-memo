25591bb95df0bce08e0b3f8b247b4c5e2dc61266
kuni <kunidesuyo1234@gmail.com>
2025-04-26 21:11:37 +0900

    ルート画面を整備

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,14 @@ export default async function Page() {
 
   return (
     <div>
-      <h1>training memo</h1>
+      <h1 className="text-4xl font-bold text-center text-gray-800">
+        Training Memo
+      </h1>
+      <div className="flex justify-center mt-4">
+        <a href="/home" className="text-blue-500 hover:text-blue-600">
+          ホームへ
+        </a>
+      </div>
     </div>
   );
 }
