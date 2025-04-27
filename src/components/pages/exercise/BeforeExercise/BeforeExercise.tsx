079883baefdcc9d4e6ebe9d57b035f1f08a39530
kuni <kunidesuyo1234@gmail.com>
2025-04-27 14:00:18 +0900

    TODO記載

--- src/components/pages/exercise/BeforeExercise/BeforeExercise.tsx
+++ src/components/pages/exercise/BeforeExercise/BeforeExercise.tsx
@@ -1,3 +1,9 @@
+// TODO: 以下仕様のコンポーネントを作成する
+// 前のエクササイズを表示する
+// コンポーネント押下時
+// L前のエクササイズが存在する場合は、前のエクササイズを表示する
+// L前のエクササイズが存在しない場合は、コンポーネントを非表示にする
+
 export default function BeforeExercise() {
   return <div>BeforeExercise</div>;
 }
