6bfe4f8d62b7f5a66a9065097f903b1d4781eaef
kuni <kunidesuyo1234@gmail.com>
2025-05-09 20:21:58 +0900

    次のエクササイズ作成dialog作成

--- src/components/pages/exercise/NextExercise/NextExercise.tsx
+++ src/components/pages/exercise/NextExercise/NextExercise.tsx
@@ -1,8 +1,18 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
 import type { Exercise } from "@/src/services/ExerciseService";
 import { ChevronRight } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function NextExercise({
   props,
@@ -14,6 +24,7 @@ export default function NextExercise({
     nextExercise: Exercise | null;
   };
 }) {
+  const [open, setOpen] = useState(false);
   const { year, month, day, nextExercise } = props;
   const router = useRouter();
 
@@ -22,7 +33,7 @@ export default function NextExercise({
       const order = nextExercise.order;
       router.push(`/workouts/${year}/${month}/${day}/exercise/${order}`);
     } else {
-      console.log("create next exercise");
+      setOpen(true);
     }
   };
 
@@ -35,6 +46,19 @@ export default function NextExercise({
       >
         <ChevronRight className="w-5 h-5" />
       </button>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>エクササイズの作成</DialogTitle>
+            <DialogDescription>
+              次のエクササイズが存在しません。作成しますか？
+            </DialogDescription>
+          </DialogHeader>
+          <DialogFooter>
+            <Button type="submit">作成する</Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
