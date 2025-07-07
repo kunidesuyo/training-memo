9ddee3c9f374a7034638cc0d6b120e258068233d
kuni <kunidesuyo1234@gmail.com>
2025-07-07 09:31:31 +0900

    想定動作環境追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ export default async function Page() {
       <h1 className="text-4xl font-bold text-center text-gray-800">
         Training Memo
       </h1>
+      <p className="text-center text-gray-600">
+        想定動作環境: iPhone 12 mini(375x812)
+      </p>
       <div className="flex justify-center mt-4">
         <a href="/home" className="text-blue-500 hover:text-blue-600">
           ホームへ
